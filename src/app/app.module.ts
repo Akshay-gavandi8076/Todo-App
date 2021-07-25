6eeddba50eb03eda2bcb27ef13fb6a0ef484281c
Akshay <Akshay.gavandi1@gmail.com>
2021-07-26 00:12:01 +0200

    CRUD Operations Done!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTodoDialogComponent } from './components/edit-todo-dialog/edit-todo-dialog.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { ToolTipDirective } from './model/tool-tip.directive';
+import { ToolTipSingletonDirective } from './model/tool-tip-singleton.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent,
+    EditTodoDialogComponent,
+    ToolTipDirective,
+    ToolTipSingletonDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DataService } from 'src/app/model/data.service';
+import { Todo } from 'src/app/model/todo.model';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTodoDialogComponent } from '../edit-todo-dialog/edit-todo-dialog.component';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos!: Todo[];
+  
+  showValidationErrors!: boolean; 
+
+constructor(private dataService: DataService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.todos = this.dataService.getAllTodos()
   }
 
+  onFormSubmit(form: NgForm) {   
+    if (form.invalid) return this.showValidationErrors = true
+
+    this.dataService.addTodo(new Todo(form.value.text))
+
+    this.showValidationErrors = false
+    form.reset()
+  }
+
+  toggleCompleted(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  editTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+
+    let dialogRef = this.dialog.open(EditTodoDialogComponent, {
+      width: '700px',
+      data: todo
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.dataService.updateTodo(index, result)
+      }
+    })
+  }
+
+  deleteTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+    this.dataService.deleteTodo(index)
+  }
+  
 }
